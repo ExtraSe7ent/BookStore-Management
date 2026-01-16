439edffad0f90d9e8952eb2ef258dd4e6d29abd4
Quang Anh <quanganhnguyn.personal@gmail.com>
2026-01-16 22:00:40 +0700

    test znx nmxz cnsdsfvadva

--- BookStore.BLL/IAuthService.cs
+++ BookStore.BLL/IAuthService.cs
@@ -1,7 +1,17 @@
 ﻿using BookStore.Entities;
 using System.Threading.Tasks; 
-
+//tétttt
 namespace BookStore.BLL
+{
+    public interface IAuthService
+    {
+        Task<User> LoginAsync(string username, string password);
+
+        Task<bool> RegisterAsync(string username, string password, string role);
+    }
+}
+
+namespace BookStore.DAL ///teéest
 {
     public interface IAuthService
     {
