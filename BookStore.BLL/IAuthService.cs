0e51d3f9530d2fd07332e4bb42eda718c6b5ca71
Quang Anh <quanganhnguyn.personal@gmail.com>
2026-01-16 22:01:59 +0700

    fix

--- BookStore.BLL/IAuthService.cs
+++ BookStore.BLL/IAuthService.cs
@@ -1,17 +1,6 @@
 ﻿using BookStore.Entities;
 using System.Threading.Tasks; 
-//tétttt
 namespace BookStore.BLL
-{
-    public interface IAuthService
-    {
-        Task<User> LoginAsync(string username, string password);
-
-        Task<bool> RegisterAsync(string username, string password, string role);
-    }
-}
-
-namespace BookStore.DAL ///teéest
 {
     public interface IAuthService
     {
